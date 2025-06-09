dd8bc0ba84992faf34930f66cdad53da37a318f2
Eryck Santos <dev.erycksantos@gmail.com>
2025-06-09 18:00:53 -0300

    Primeiro esboço. Replicando página inicial do Google.

---
+++ src/main/java/tech/buildrun/spotify/controller/AlbumController.java
@@ -0,0 +1,23 @@
+package tech.buildrun.spotify.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.buildrun.spotify.client.GoogleClient;
+
+@RestController
+@RequestMapping("/spotify/api")
+public class AlbumController {
+
+    private final GoogleClient googleClient;
+
+    public AlbumController(GoogleClient googleClient) {
+        this.googleClient = googleClient;
+    }
+
+    @GetMapping("/albuns")
+    public ResponseEntity<String> helloWorld(){
+        return ResponseEntity.ok(googleClient.helloWorld());
+    }
+}
